0df93dc95a964ee357b5ccd6f571e583a678fb81
kike1357 <quique_8_94@hotmail.com>
2017-02-15 22:08:58 +0100

    Comunicación entre activitys realizada

--- app/src/main/java/com/example/pablorodriguez/fragments/Main2ActivityFragment.java
+++ app/src/main/java/com/example/pablorodriguez/fragments/Main2ActivityFragment.java
@@ -1,10 +1,15 @@
 package com.example.pablorodriguez.fragments;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +22,28 @@ public class Main2ActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main2, container, false);
+
+        View vistaFrag2=inflater.inflate(R.layout.fragment_main2, container, false);
+
+        TextView texto2=(TextView)vistaFrag2.findViewById(R.id.textoFrag2);
+        String str=getActivity().getIntent().getStringExtra("Activity1");
+
+        texto2.setText(str);
+        Button botonEnviar=(Button)vistaFrag2.findViewById(R.id.botonFrag2);
+
+        botonEnviar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intnt=new Intent();
+                intnt.putExtra("Activity2","Estamos en la activity 2");
+                getActivity().setResult(RESULT_OK,intnt);
+                getActivity().finish();
+
+            }
+        });
+
+        return vistaFrag2;
     }
 }
--- app/src/main/java/com/example/pablorodriguez/fragments/MainActivityFragment.java
+++ app/src/main/java/com/example/pablorodriguez/fragments/MainActivityFragment.java
@@ -1,22 +1,67 @@
 package com.example.pablorodriguez.fragments;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
+    Button boton;
+    TextView textoVista;
+    public static final int CODIGO = 1;
     public MainActivityFragment() {
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View vistaFrag1 = inflater.inflate(R.layout.fragment_main, container, false);
+
+        boton = (Button) vistaFrag1.findViewById(R.id.boton);
+        textoVista = (TextView) vistaFrag1.findViewById(R.id.texto);
+
+
+        boton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Intent i = new Intent(getActivity(), Main2Activity.class);
+                i.putExtra("Activity1", "Estamos en la activity 1");
+
+                startActivityForResult(i, CODIGO);
+            }
+        });
+
+        return vistaFrag1;
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if (requestCode == CODIGO) {
+
+            if (resultCode == RESULT_OK) {
+
+                String result = data.getStringExtra("Activity2");
+                textoVista.setText(result);
+
+            }
+
+        }
     }
 }
